afa60816ba3cdce302acf3e373775cf6f9cd844e
luanws <luan.w.silveira@gmail.com>
2022-04-03 16:21:45 -0300

    Create 4_variables.js

---
+++ src/1_basic/4-variables.js
@@ -0,0 +1,30 @@
+// Existem 3 maneiras de declarar variáveis em javascript
+// const, let e var
+
+// const
+// const não pode ser alterada
+// const não pode ser redeclarada
+
+const iNeverChange = 'Eu nunca mudo'
+
+// let
+// let ser alterada
+// let não pode ser redeclarada
+
+let iChange = 'Eu mudo'
+iChange = 'Eu acabei de mudar'
+
+// var
+// var pode ser alterada
+// var pode ser redeclarada
+// var não possui escopo de bloco
+// var não é recomendado
+// não use var
+// não use mesmo
+// não é pra usar var
+// var não é seu amigo
+// var vai dar erro
+
+var iChangeWithVar = 'Eu mudo'
+iChangeWithVar = 'Eu acabei de mudar'
+var iChangeWithVar = 'Eu acabei de ser redeclarado'
