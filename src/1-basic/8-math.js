1115f69b4043535de94e1fb7af1a7a890ce8d004
luanws <luan.w.silveira@gmail.com>
2022-04-03 18:40:44 -0300

    Create 8-math.js

---
+++ src/1-basic/8-math.js
@@ -0,0 +1,43 @@
+// Math é um objeto global que contém métodos matemáticos.
+
+// Módulo
+Math.abs(-2) // 2
+Math.abs(2) // 2
+
+// Raiz quadrada
+Math.sqrt(4) // 2
+
+// Potência
+Math.pow(2, 3) // 8
+
+// Funções trigonométricas
+Math.sin(0) // 0
+Math.cos(0) // 1
+Math.tan(0) // 0
+
+// Arredondamento
+Math.round(2.5) // 3
+Math.round(2.4) // 2
+Math.round(2.6) // 3
+
+// Arredondamento para cima
+Math.ceil(2.5) // 3
+Math.ceil(2.4) // 3
+Math.ceil(2.6) // 3
+
+// Arredondamento para baixo
+Math.floor(2.5) // 2
+Math.floor(2.4) // 2
+Math.floor(2.6) // 2
+
+// Aleatório
+Math.random() // Número aleatório entre 0 e 1
+Math.random() * 10 // Número aleatório entre 0 e 10
+Math.random() * (10 - 5) + 5 // Número aleatório entre 5 e 10
+
+// Maior e menor
+Math.max(1, 2, 3) // 3
+Math.min(1, 2, 3) // 1
+
+// Pi
+Math.PI // 3.141592653589793
