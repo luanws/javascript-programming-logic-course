1bc817d99255df85df724a4080ddfe64a62d0375
luanws <luan.w.silveira@gmail.com>
2022-04-03 15:46:40 -0300

    Create 2-program-structure.js

---
+++ src/1_basic/2-program-structure.js
@@ -0,0 +1,9 @@
+// Não há estrutura de programação em javascript
+// O código é executado de forma linear a partir de um arquivo
+
+// O arquivo package.json é o arquivo de configuração do projeto
+// O arquivo package-lock.json é o arquivo de dependências do projeto
+// A pasta node_modules é a pasta onde são armazenadas as dependências do projeto
+
+// Para instalar as dependências do projeto, execute o comando:
+// > npm install
