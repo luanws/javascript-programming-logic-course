5715f29f2b276a27720e9eeb7f7bbe439d482eaf
luanws <luan.w.silveira@gmail.com>
2022-04-04 16:26:00 -0300

    Create 5-ternary-operator.js

---
+++ src/2-conditions/5-ternary-operator.js
@@ -0,0 +1,6 @@
+const condition = true
+const valueIfConditionIsTrue = 3
+const valueIfConditionIsFalse = 10
+
+const variable = condition ? valueIfConditionIsTrue : valueIfConditionIsFalse
+console.log(variable)
