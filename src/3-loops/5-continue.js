0be5263f0c80ec7ad2522e1855abb5b8d0c54f9d
luanws <luan.w.silveira@gmail.com>
2022-04-04 17:06:21 -0300

    Create 5-continue.js

---
+++ src/3-loops/5-continue.js
@@ -0,0 +1,8 @@
+// O continue serve para pular o resto do bloco de repetição e continuar com a próxima iteração.
+
+for (let i = 0; i < 5; i++) {
+    if (i === 3) {
+        continue
+    }
+    console.log(i)
+}
