c7c439961276ec659fb92cdbaf5fc5f7b7376525
luanws <luan.w.silveira@gmail.com>
2022-04-06 15:00:10 -0300

    Create 3-for-each.js

---
+++ src/4-arrays/3-for-each.js
@@ -0,0 +1,5 @@
+// O for each executa uma função para cada elemento do array.
+// Funciona de uma forma muito semelhante ao for of.
+
+const array = [1, 2, 3, 4, 5]
+array.forEach(element => console.log(element)) // 1 2 3 4 5
