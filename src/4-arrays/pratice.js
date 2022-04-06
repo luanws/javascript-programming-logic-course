102865c2c3bc8a20c51c24796cc2b5bfad329138
luanws <luan.w.silveira@gmail.com>
2022-04-06 15:12:51 -0300

    pratice

--- src/4-arrays/pratice.js
+++
@@ -1,9 +0,0 @@
-// Recrie a função map que recebe um array e uma função de callback,
-// e retorna um novo array com os resultados das chamadas da função de callback.
-
-// Exemplo:
-map([1, 2, 3], x => item * 2) // [2, 4, 6]
-
-// Crie uma função customMap e adicione ao seu objeto Array.prototype
-// através do prototype.
-// Array.prototype.customMap = customMap
---
+++ src/6-objects/4-pratice.js
@@ -0,0 +1,8 @@
+// Crie um menu de opções com as seguintes opções:
+// 1- Soma
+// 2- Subtração
+// 3- Multiplicação
+// 4- Divisão
+// 5- Sair
+// Escolha uma opção e informe o resultado da operação.
+// Você não pode usar if else ou switch case.
