76530253b78962c632d418da2ad6bb30415974b9
luanws <luan.w.silveira@gmail.com>
2022-04-06 16:55:13 -0300

    Update 5-pratice.js

--- src/5-functions/5-pratice.js
+++ src/5-functions/5-pratice.js
@@ -2,8 +2,18 @@
 // e retorna um novo array com os resultados das chamadas da função de callback.
 
 // Exemplo:
-map([1, 2, 3], x => item * 2) // [2, 4, 6]
+[1, 2, 3].map(x => x * 2) // [2, 4, 6]
 
 // Crie uma função customMap e adicione ao seu objeto Array.prototype
 // através do prototype.
 // Array.prototype.customMap = customMap
+
+function customMap(callback) {
+    // Implementar essa função
+}
+
+Array.prototype.customMap = customMap
+
+const array = [1, 2, 3]
+const x = array.customMap(x => x * 2)
+console.log(x) // [2, 4, 6]
