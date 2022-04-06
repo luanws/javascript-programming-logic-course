e2946d748caee2cfcf71ae2b174bbadd95571323
luanws <luan.w.silveira@gmail.com>
2022-04-06 16:14:20 -0300

    Update 1-functions.js

--- src/5-functions/1-functions.js
+++ src/5-functions/1-functions.js
@@ -2,6 +2,14 @@
 // função, função arrow e função anônima
 
 // Função
-function sumFunction(a, b) {
+function sum(a, b) {
     return a + b
 }
+
+// Funções podem receber um parâmetro com valor padrão
+function sumWithDefault(a, b = 1) {
+    return a + b
+}
+
+sumWithDefault(2, 9) // 11
+sumWithDefault(2) // 3
