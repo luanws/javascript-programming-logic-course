7b6699a9215d9143bcac99a1ed1f5fc6cb603143
luanws <luan.w.silveira@gmail.com>
2022-04-06 15:08:10 -0300

    Update 1-functions.js

--- src/5-functions/1-functions.js
+++ src/5-functions/1-functions.js
@@ -1,29 +1,7 @@
 // Existem 3 maneiras de declarar funções em JavaScript
 // função, função arrow e função anônima
 
-// função
+// Função
 function sumFunction(a, b) {
     return a + b
 }
-
-// função arrow
-const sumArrowFunction = (a, b) => a + b
-
-// função anônima
-const sumAnonymousFunction = function (a, b) {
-    return a + b
-}
-
-// Existem diferenças entre funções e funções arrow
-// O this não é definido em funções arrow
-// Mas é definido em funções
-
-// Exemplo
-const person = {
-    name: 'João',
-    age: 20,
-    sayName: function () {
-        console.log(this.name)
-    },
-}
-console.log(person.sayName()) // João
