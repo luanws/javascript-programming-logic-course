00f27f2b494db5378d7b1784693320bb6ac4179d
luanws <luan.w.silveira@gmail.com>
2022-04-06 16:05:09 -0300

    Update 5-filter.js

--- src/4-arrays/5-filter.js
+++ src/4-arrays/5-filter.js
@@ -1,4 +1,5 @@
 // O filter retorna um novo array com os elementos que passaram no teste
+// O filter literalmente filtra o array para retornar alguns elementos desse array
 
 const array = [1, 2, 3, 4, 5]
 array.filter(element => element % 2 === 0) // [2, 4]
