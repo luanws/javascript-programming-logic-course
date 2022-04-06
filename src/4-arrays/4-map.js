93a6ce760972a828c4453146d8d330a1f5bdf5a2
luanws <luan.w.silveira@gmail.com>
2022-04-06 16:03:58 -0300

    Update 4-map.js

--- src/4-arrays/4-map.js
+++ src/4-arrays/4-map.js
@@ -1,4 +1,5 @@
 // O map retorna um novo array com os resultados da função passada como parâmetro
+// O map sempre retorna um array de mesmo tamanho do array original
 
 const array = [1, 2, 3, 4, 5]
 array.map(element => element * 2) // [2, 4, 6, 8, 10]
