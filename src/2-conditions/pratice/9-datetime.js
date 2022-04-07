f0a4cc3a7fb3891aa34ca286a6be1f69d9795794
luanws <luan.w.silveira@gmail.com>
2022-04-07 17:18:46 -0300

    Create 9-datetime.js

---
+++ src/2-conditions/pratice/9-datetime.js
@@ -0,0 +1,4 @@
+// Fazer um algoritmo que leia valores para as variáveis hora, minuto e segundo
+// verificando e escrevendo se elas correspondem a um horário valido ou não. Para um
+// horário ser valido, deve estar no intervalo de 0 a 23, o minuto e o segundo no
+// intervalo de 0 a 59.
