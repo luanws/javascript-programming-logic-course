fb39a46f007722fff196fc1fdaa1faaea85ef768
luanws <luan.w.silveira@gmail.com>
2022-04-07 17:27:19 -0300

    Create 10-triangles.js

---
+++ src/2-conditions/pratice/10-triangles.js
@@ -0,0 +1,23 @@
+// Fazer um algoritmo que leia e verifique se três valores (a,b,c) podem ser os 
+// comprimentos dos lados de um triangulo, escrevendo o tipo do triangulo (retângulo, 
+// obtusângulo, acutângulo, equilátero, isósceles, escaleno).
+
+// A definição dos triângulos está na tabela abaixo:
+
+// |---------------------|------------------------------------------------------------------------------------|
+// |  TIPO DE TRIÂNGULO  |  COMPRIMENTOS DOS LADOS                                                            |
+// |---------------------|------------------------------------------------------------------------------------|
+// |  Não é triângulo    |  A soma dos lados menores é menor ou igual ao lado maior                           |
+// |---------------------|------------------------------------------------------------------------------------|
+// |  Retângulo          |  O quadrado do maior dos lados é igual a soma dos quadrados dos outros lados       |
+// |---------------------|------------------------------------------------------------------------------------|
+// |  Obtusângulo        |  O quadrado do maior dos lados é maior que a soma dos quadrados dos outros lados   |
+// |---------------------|------------------------------------------------------------------------------------|
+// |  Acutângulo         |  O quadrado do maior dos lados é menor que a soma dos quadrados dos outros lados   |
+// |---------------------|------------------------------------------------------------------------------------|
+// |  Equilátero         |  Todos os lados são iguais                                                         |
+// |---------------------|------------------------------------------------------------------------------------|
+// |  Isósceles          |  Somenos dois lados são iguais                                                     |
+// |---------------------|------------------------------------------------------------------------------------|
+// |  Escaleno           |  Todos os lados são diferentes                                                     |
+// |---------------------|------------------------------------------------------------------------------------|
