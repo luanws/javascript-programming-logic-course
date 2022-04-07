f6902c2628cdd18ef96b81ed14e23cdc7ef463db
luanws <luan.w.silveira@gmail.com>
2022-04-07 17:11:39 -0300

    Create 5-month-number.js

---
+++ src/2-conditions/pratice/5-month-number.js
@@ -0,0 +1,3 @@
+// Faça um algoritmo que leia um número inteiro e diga a qual mês do ano o mesmo 
+// corresponde. Se o valor for maior que doze ou menor que um, diga que o valor não 
+// corresponde a nenhum mês. Ex.: 3 = mês de março.
