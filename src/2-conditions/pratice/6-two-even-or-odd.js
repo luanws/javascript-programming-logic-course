41db2c23a46db81ebad5e290e2be25530c346dd5
luanws <luan.w.silveira@gmail.com>
2022-04-07 17:13:19 -0300

    Create 6-two-even-or-odd.js

---
+++ src/2-conditions/pratice/6-two-even-or-odd.js
@@ -0,0 +1,6 @@
+// A partir de dois números fornecidos pelo usuário, escrever uma
+// das seguintes mensagens: 
+// a) Os dois são pares 
+// b) Os dois são ímpares 
+// c) O primeiro é par e o segundo é ímpar 
+// d) O primeiro é ímpar e o segundo é par 
