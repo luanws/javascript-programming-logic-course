364da4b9d11bf79758708320a97054b529c51084
luanws <luan.w.silveira@gmail.com>
2022-04-07 22:44:43 -0300

    Create 10-guessing-game.js

---
+++ src/3-loops/pratice/10-guessing-game.js
@@ -0,0 +1,5 @@
+// Faça um programa que escolhe um número aleatório entre 1 e 10.
+// O usuário deve tentar adivinhar o número.
+// Se o usuário adivinhar, o programa deve informar que o usuário acertou.
+// Se o usuário não adivinhar, o programa deve informar se o número é maior ou menor que o número escolhido.
+// Ao final deve ser informado quantas tentativas foram necessárias para adivinhar o número.
