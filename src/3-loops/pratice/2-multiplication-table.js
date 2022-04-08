ff167ac9ce468506fe6fbe73412ac33852a65d95
luanws <luan.w.silveira@gmail.com>
2022-04-07 22:23:42 -0300

    Create 2-multiplication-table.js

---
+++ src/3-loops/pratice/2-multiplication-table.js
@@ -0,0 +1,8 @@
+// Desenvolver um algoritmo que leia um número inteiro e apresente a tabuada da 
+// multiplicação no seguinte formato:
+// Exemplo: 
+// valor lido: 2
+// 2 x 0 = 0 
+// 2 x 1 = 1
+// ...
+// 2 x 10 = 20
