ba769ca7dee2449e195715445c140df9f88e3cb9
luanws <luan.w.silveira@gmail.com>
2022-04-07 22:41:03 -0300

    Create 9-draw-square.js

---
+++ src/3-loops/pratice/9-draw-square.js
@@ -0,0 +1,13 @@
+// Faça um programa que desenhe um quadrado de zeros com a dimensão
+// informada pelo usuário.
+// Exemplo:
+// Entrada:
+// 5
+// Saída:
+// 0 0 0 0 0
+// 0 0 0 0 0
+// 0 0 0 0 0
+// 0 0 0 0 0
+// Dica: strings podem ser concatenadas com o operador +.
+// Exemplo:
+// 'Olá ' + 'mundo!' == 'Olá mundo!'
